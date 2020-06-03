137036b1d6976bda46367253555c4c3aaf760b15
Bayu Setiawan <61371193+bayusetiawan01@users.noreply.github.com>
2020-06-03 20:40:19 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -139,8 +139,8 @@ class Auth extends CI_Controller
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'ssl://smtp.googlemail.com';            // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'absensipraktikum.mtk@gmail.com';       // SMTP username
-        $mail->Password   = 'mataikan123';                          // SMTP password
+        $mail->Username   = 'EMAIL';                                // SMTP username
+        $mail->Password   = 'PASSWORD';                             // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
